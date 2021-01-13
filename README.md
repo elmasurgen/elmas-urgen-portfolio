3ba1394c96fb0c88b019edd35f05ccd6ea02704b
elmasurgen <elmas.urgen@gmail.com>
2021-01-13 11:56:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# elmas-urgen-portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/elmas-urgen-portfolio)
---
+++ src/pages/Portfolio/portfolioData.js
@@ -0,0 +1,42 @@
+export const portfolioData = [
+  {
+    id: 0,
+    title: "Movie DB",
+    techs: [
+      `fab fa-react`,
+      `fab fa-css3-alt mx-3`,
+      `fab fa-js mx-3`,
+      `fab fa-bootstrap`
+    ],
+    imgUrl:"https://i.resmim.net/H2SPT.png",
+    github: "https://github.com/elmasurgen/MovieDB",
+    live: "https://elmasurgen-moviedb.netlify.app/"
+  },
+  {
+    id: 1,
+    title: " Context API Project",
+    techs: [
+      `fab fa-react`,
+      `fab fa-css3-alt mx-3`,
+      `fab fa-js mx-3`,
+      `fab fa-bootstrap`
+    ],
+    imgUrl: "https://i.resmim.net/H2ypr.png",
+    github: "https://github.com/elmasurgen/react-contextapi",
+    live: "https://elmas-urgen-contextapi.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "Soil&Pot",
+    techs: [
+      `fab fa-html5`,
+      `fab fa-css3-alt mx-3`,
+      `fab fa-js mx-3`,
+      `fab fa-bootstrap`
+    ],
+    imgUrl:  "https://i.resmim.net/H21d7.png ",
+    github: "https://github.com/elmasurgen/Soil-pot",
+    live: "https://elmasurgen-soilpot.netlify.app/"
+  },
+ 
+];
